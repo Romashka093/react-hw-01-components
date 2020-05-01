480c52a074bfef7d3ee133d750268fdecb19c92a
Anna Romashenko <aa.romashchenko@gmail.com>
2020-05-01 11:56:16 +0300

    Updates

---
+++ precache-manifest.a89af7642d5ac27967a479d7f519d3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ff7835a1fd9de270db11a2c8ff6b02",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "de8bb7d325890743a9c4",
+    "url": "/goit-react-hw-01-components/static/css/main.60f02131.chunk.css"
+  },
+  {
+    "revision": "d90dfdf55669d29adcd6",
+    "url": "/goit-react-hw-01-components/static/js/2.fa9e5ae3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.fa9e5ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8bb7d325890743a9c4",
+    "url": "/goit-react-hw-01-components/static/js/main.3f61e08a.chunk.js"
+  },
+  {
+    "revision": "e6e52a6c8a65f3c70b2a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.69b22fd4.js"
+  }
+]);
